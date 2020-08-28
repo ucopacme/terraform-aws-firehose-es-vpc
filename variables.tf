08af7a5470b5d3250e6adf2e411832288a9be257
Khalid Ahmadzai <khalid.ahmadzai@ucop.edu>
2020-08-28 13:32:30 -0700

    updated the var files

---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "bucket_name" {
+  description = "bucket name"
+  type        = string
+}
+
+variable "delivery_stream_name" {
+  description = "delivery stream name"
+  type        = string
+}
+
+variable "domain_arn" {
+  description = "elasticsearch domain arn"
+  type        = string
+}
+
+variable "enabled" {
+  description = "to be or not to be"
+  default     = true
+  type        = bool
+}
+
+variable "index_name" {
+  description = "elasticsearch index name"
+  type        = string
+}
+
+variable "security_group" {
+  description = "security group to access elastic search domain"
+  type        = string
+}
+
+variable "stack_name" {
+  description = "name of cloudformation stack"
+  type        = string
+}
+
+variable "subnet_ids" {
+  default     = []
+  description = "subnet ids"
+  type        = list(string)
+}
